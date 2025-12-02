b142b570b340d2c8c90fe73e3ca999b7ca460241
SamuelPineda <samuelepinedab2206@gmail.com>
2025-12-01 23:11:33 -0500

    2 commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/SamuelPineda1.github.io"
+  base: "/SamuelPineda1"
 })
